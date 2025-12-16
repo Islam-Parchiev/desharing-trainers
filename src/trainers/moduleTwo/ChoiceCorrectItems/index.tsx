5065980a65676965038e237540c7e7ed712cfa2f
Islam Parchiev <IslamParchievv@yandex.ru>
2025-12-16 11:25:51 +0300

    ChoiceCorrectItems

--- src/trainers/moduleTwo/ChoiceCorrectItems/index.tsx
+++ src/trainers/moduleTwo/ChoiceCorrectItems/index.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './styles.scss';
 import { TrainerTitle } from '../../../components/TrainerTitle';
-import type { Id } from '../../../types/types';
+import type { Id, Status } from '../../../types/types';
 import cn from 'classnames';
 
 interface Item {
     id: Id;
     value: string;
-    correct: boolean;
     position: number;
     submitted?: boolean;
 }
 
 const mockData = {
     data: [
-        { id: 1, value: "test1", correct: false, position: 1 },
-        { id: 2, value: "ехать", correct: true, position: 2 },
-        { id: 3, value: "г1рп", correct: false, position: 3 },
-        { id: 4, value: "поле", correct: true, position: 4 }, // Изменено position с 7 на 4
-        { id: 5, value: "тр-пм", correct: false, position: 5 },
-        { id: 6, value: "сказка", correct: true, position: 6 }, // Изменено position с 7 на 6
-        { id: 7, value: "test7", correct: false, position: 7 }
+        { id: 1, value: "test1", position: 1 },
+        { id: 2, value: "ехать", position: 2 },
+        { id: 3, value: "г1рп", position: 3 },
+        { id: 4, value: "поле", position: 4 },
+        { id: 5, value: "тр-пм", position: 5 },
+        { id: 6, value: "сказка", position: 6 },
+        { id: 7, value: "test7", position: 7 }
+    ],
+    correctVariants: [
+        { id: 11, value: "ехать" },
+        { id: 14, value: "поле" },
+        { id: 16, value: "сказка" }
     ]
 };
 
 export const ChoiceCorrectItems = () => {
-    // Используем state для отслеживания submitted состояния
     const [items, setItems] = useState<Item[]>(
         mockData.data.map(item => ({ ...item, submitted: false }))
     );
-
+    const [correct, setCorrect] = useState<{ value: string; }[]>(mockData.correctVariants);
+    const [submittedItems, setSubmittedItems] = useState<Item[]>([]);
+    const [status, setStatus] = useState<Status>("idle");
     const totalSlots = 9;
     const slots = Array.from({ length: totalSlots }, (_, i) => i + 1);
     const firstRowSlots = slots.slice(0, 4);
@@ -38,9 +43,18 @@ export const ChoiceCorrectItems = () => {
     const getItemByPosition = (position: number) => {
         return items.find(item => item.position === position);
     };
+    const handleCheck = () => {
+        const submitted = items.filter(item => item.submitted);
 
+        if (submitted.length !== 3) return;
+
+        const isCorrect = submitted.every(item =>
+            correct.some(correctItem => correctItem.value === item.value)
+        );
+
+        setStatus(isCorrect ? "success" : "error");
+    }
     const handleItemClick = (item: Item) => {
-        // Не обновляем напрямую, используем setItems
         setItems(prevItems =>
             prevItems.map(prevItem =>
                 prevItem.id === item.id
@@ -49,10 +63,16 @@ export const ChoiceCorrectItems = () => {
             )
         );
     };
-
+    useEffect(() => {
+        handleCheck();
+    }, [items])
     const addClass = (item: Item) => {
         if (item.submitted) {
-            return item.correct ? "hidden" : "error";
+            if (correct.find(el => el.value === item.value)) {
+                return "hidden";
+            } else {
+                return "error";
+            }
         }
         return "";
     };
@@ -61,9 +81,10 @@ export const ChoiceCorrectItems = () => {
         <div className="ChoiceCorrectItems">
             <div className="ChoiceCorrectItems__inner">
                 <TrainerTitle>Выбери ТРИ яблока со словами на русском языке</TrainerTitle>
+                {status === "error" && "Error"}
+                {status === "success" && "Success"}
                 <div className="ChoiceCorrectItems__content">
                     <div className="ChoiceCorrectItems__apples">
-                        {/* Первая строка */}
                         <div className="ChoiceCorrectItems__row">
                             {firstRowSlots.map(slotPosition => {
                                 const item = getItemByPosition(slotPosition);
@@ -82,8 +103,6 @@ export const ChoiceCorrectItems = () => {
                                 );
                             })}
                         </div>
-
-                        {/* Вторая строка */}
                         <div className="ChoiceCorrectItems__row">
                             {secondRowSlots.map(slotPosition => {
                                 const item = getItemByPosition(slotPosition);
