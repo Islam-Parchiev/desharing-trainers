2281c96affdf00cfd841fb5dab2fa80bd2560a72
Islam Parchiev <IslamParchievv@yandex.ru>
2026-01-23 09:45:35 +0300

    add:  FindMistake trainer

--- src/trainers/moduleThree/FindMistake/Word/index.tsx
+++ src/trainers/moduleThree/FindMistake/Word/index.tsx
@@ -1,26 +1,45 @@
 import { useState } from "react";
 import cn from 'classnames';
 
+import './styles.scss';
+import { Button } from "../../../../shared/ui/Button";
+
 interface WordWithMistakeProps { content: string; variants: string[]; correctVariant: string; handleSuccess: () => void; handleError: () => void; }
 
 export const WordWithMistake = ({ content, correctVariant, variants, handleSuccess, handleError }: WordWithMistakeProps) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentContent, setCurrentContent] = useState(content);
+    const [isSubmitted, setIsSubmitted] = useState(false);
     const handleVariantClick = (v: string) => {
-        if (v === correctVariant) {
-            handleSuccess()
-        } else {
-            handleError()
+        if (!isSubmitted) {
+
+            if (v === correctVariant) {
+                handleSuccess()
+                setCurrentContent(v);
+                setIsModalOpen(false);
+                setIsSubmitted(true);
+            } else {
+                handleError()
+            }
+        }
+        return;
+    }
+    const handleOpenModal = () => {
+        if (!isSubmitted) {
+            setIsModalOpen(true);
         }
         return;
     }
     return <div className="WordWithMistake">
-        {isModalOpen && variants ?
-            <div className="WordWithMistake-variantsModal">
-                {variants.map(variant => <span className="WordWithMistake__variant" onClick={() => handleVariantClick(variant)}>{variant}</span>)}
-            </div> : <>something went wrong</>
+        {variants ? isModalOpen && <div className="WordWithMistake-variantsModal">
+            {variants.map(variant => <Button variant="primary" size="small" asChild className="WordWithMistake__variant" onClick={() => handleVariantClick(variant)}><span>
+                {variant}
+            </span>
+            </Button>)}
+        </div> : <>something went wrong</>
         }
-        <span className={cn('WordWithMistake__content', isModalOpen ? 'modal-opened' : '')} onClick={() => setIsModalOpen(true)}>
-            {content}
+        <span className={cn('WordWithMistake__content', isModalOpen ? 'modal-opened' : '')} onClick={handleOpenModal}>
+            {currentContent}
         </span>
     </div>
 }
--- src/trainers/moduleThree/FindMistake/index.tsx
+++ src/trainers/moduleThree/FindMistake/index.tsx
@@ -1,31 +1,98 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TrainerTitle } from '../../../components/TrainerTitle';
 import './styles.scss';
 import { WordWithMistake } from './Word';
 import type { Status } from '../../../types/types';
-const mockData = {
-    content: "В старом доме есть {{горячая}} вода.",
 
+const mockData = {
+    trainerTitle: "Найди ошибку",
+    content: "В старом доме есть {{горячяя}} {{вада.}}",
+    words: [
+        {
+            content: "горячяя",
+            variants: ["горячаа", "горячая"],
+            correctVariant: 'горячая'
+        },
+        {
+            content: "вада.",
+            variants: ["вода.", "водя."],
+            correctVariant: 'вода.'
+        }
+    ]
 }
+
 export const FindMistake = () => {
     const [status, setStatus] = useState<Status>("idle");
+    const [words, setWords] = useState(mockData.words.map(item => ({
+        ...item,
+        submitted: false
+    })));
+    const [data] = useState(mockData);
+
+    const submittedOfWord = (value: string) => {
+        setWords(prevWords =>
+            prevWords.map(item =>
+                item.content === value
+                    ? { ...item, submitted: true }
+                    : item
+            )
+        );
+    };
+
+    const handleWordSuccess = (content: string) => {
+        submittedOfWord(content);
+    };
+
+    const handleWordError = () => {
+        setStatus("error");
+    };
+
+    const renderContent = (sentence: string) => {
+        const parts = sentence.split(/\{\{([^{}]+)\}\}/g);
+
+        return parts.map((part, index) => {
+            const word = words.find(w => w.content === part);
+
+            if (word) {
+                return (
+                    <WordWithMistake
+                        key={`WordWithMistake-${word.content}-${index}`}
+                        content={word.content}
+                        correctVariant={word.correctVariant}
+                        variants={word.variants}
+                        handleError={handleWordError}
+                        handleSuccess={() => handleWordSuccess(word.content)}
+                    />
+                );
+            }
+
+            return <span key={`text-${index}`}>{part}</span>;
+        });
+    };
+
+    const checkCompletion = () => {
+        if (words.every(word => word.submitted)) {
+            setStatus('success');
+        }
+    };
+
+    useEffect(() => {
+        checkCompletion();
+    }, [words]);
+
     return (
         <div className='FindMistake'>
             <div className="FindMistake__inner">
-                <TrainerTitle>Найди ошибку</TrainerTitle>
+                <TrainerTitle>{data.trainerTitle}</TrainerTitle>
                 <h3>Нажми на слово, которое написано неправильно</h3>
-                {status === "success" && "Success"}
-                {status === "error" && "Error"}
+
+                {status === "success" && <div className="success-message">Success</div>}
+                {status === "error" && <div className="error-message">Error</div>}
+
                 <div className="FindMistake__content">
-                    В старом доме есть горячая вода.
+                    {renderContent(data.content)}
                 </div>
-                <WordWithMistake
-                    content='горячяя'
-                    correctVariant='гарячая'
-                    variants={["горячаа", "горячая"]}
-                    handleError={() => setStatus("error")}
-                    handleSuccess={() => setStatus("success")} />
             </div>
         </div>
-    )
-}
+    );
+};
